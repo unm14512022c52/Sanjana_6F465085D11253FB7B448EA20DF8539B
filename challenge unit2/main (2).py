561c1b8fad31c9baedfe9f0d4810008fe385b467
unm14512022c52 <148646784+unm14512022c52@users.noreply.github.com>
2023-10-21 23:55:33 +0530

    Add files via upload

---
+++ challenge unit2/main (2).py
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+class Player:
+  def play(self):
+    print("player is playing  cricket.")
+
+
+class Batsman(Player):
+  def play(self):
+    print("The batsman is batting.")
+
+
+class Bowler(Player):
+  def play(self):
+    print("The bowler is bowling.")
+
+
+batsman=Batsman()
+bowler=Bowler()
+
+
+batsman.play()
+bowler.play()
+
+    
+
+
+
+    
