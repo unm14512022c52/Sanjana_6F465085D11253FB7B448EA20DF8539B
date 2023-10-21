f19de11bc076fa38a2060bb1ddaa977267e06039
unm14512022c52 <148646784+unm14512022c52@users.noreply.github.com>
2023-10-21 23:55:58 +0530

    Add files via upload

---
+++ challenge unit3/main (5).py
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+class BankAccount:
+
+  def __init__(self,account_number, account _holder_name,initial_balance=0.0):
+  self.__account_number = account _number
+  self.__account_holder_name = account_holder_name
+  self.__account_balance = initial_balance
+
+def deposit(self,amount):
+  if amount > 0:
+    self.__account_balance += amount
+  #self.__account_balance = self.__account_balance+account
+  print("Deposited {}. New balance: {}".format(amount,self.__account_balance))
+  
+  else:
+    print("Invalid deposit qmount. Please deposit a positive amount.")
+
+def withdraw(self,amount):
+  if amount > 0 and amount <= self.__account_balance:
+    self.__account_balance -= amount
+    #self.__account_balance = self.__account_balance-amount
+    print("withdrew {}. New balance: {}".format(amount,self.__account_balance))
+
+  
+  else:
+    print("Invalid withdraw amount or insufficient balance")
+
+def display_balance(self):
+  print("Account balancr for {} (Account #{}): {}".format(
+    self.__account_holder_name,self.__account_number
+    self.__account_balance
+
+
+#create an instance of the BackAccount class 
+account = BankAccount(account_number="123456789",
+account_holder_name="sanjana",
+initial_balance=5000.0)
+
+#Test deposit and withdrawal functionality
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+#account.display_balance()
